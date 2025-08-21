631ad22587a26134a470b6db4608909219a3252e
prasadvakada007 <prasadvakada2@gmail.com>
2025-08-22 00:09:05 +0530

    add some modified

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return {"message": "Hello from Dockerized Python app!"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
